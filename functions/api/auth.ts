1bb672ccb6acb4d2572e0a21b024e8d2ba262d97
unknown <zatops@qq.com>
2026-05-25 16:27:25 +0800

    Use OAuth endpoint with Cloudflare Pages Functions

---
+++ functions/api/auth.ts
@@ -0,0 +1,54 @@
+export async function onRequest(context) {
+  const { request, env } = context;
+  const url = new URL(request.url);
+  const clientId = env.GITHUB_CLIENT_ID;
+  const clientSecret = env.GITHUB_CLIENT_SECRET;
+
+  if (url.searchParams.get('code')) {
+    const code = url.searchParams.get('code');
+    const response = await fetch('https://github.com/login/oauth/access_token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        client_id: clientId,
+        client_secret: clientSecret,
+        code: code,
+      })
+    });
+
+    const data = await response.json();
+    const accessToken = data.access_token;
+
+    return new Response(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <meta charset="utf-8">
+      <title>正在登录...</title>
+    </head>
+    <body>
+      <script>
+        window.opener && window.opener.postMessage({
+          type: 'authorization',
+          provider: 'github',
+          token: '${accessToken}',
+        }, window.location.origin);
+        setTimeout(() => window.close(), 500);
+      </script>
+      <p>授权成功！正在返回后台...</p>
+    </body>
+    </html>
+  `, {
+      headers: {
+        'Content-Type': 'text/html; charset=utf-8',
+      },
+    });
+  }
+  return Response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=public_repo,user`,
+    302
+  );
+}
